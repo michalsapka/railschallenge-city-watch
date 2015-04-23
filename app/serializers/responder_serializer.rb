a9d4ef77eee28422793d6851866b8df5dff02d70
Michal Sapka <msapka@gmail.com>
2015-04-23 19:20:39 +0200

    fixed serialer
    used to reference removed colum emergency_code (removed by #1)

--- app/serializers/responder_serializer.rb
+++ app/serializers/responder_serializer.rb
@@ -1,3 +1,7 @@
 class ResponderSerializer < ActiveModel::Serializer
   attributes :type, :name, :capacity, :on_duty, :emergency_code
+
+  def emergency_code
+    object.emergency.first.code if object.emergency
+  end
 end
