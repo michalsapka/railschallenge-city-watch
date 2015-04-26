24bd2ad9eff209ab44bfa45e4a25e903886d6035
Michal Sapka <msapka@gmail.com>
2015-04-26 22:06:07 +0200

    shorter routes
    used except in place of only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :responders, only: [:index, :create, :show, :update], constraints: { format: 'json' }
-  resources :emergencies, only: [:index, :create, :show, :update], constraints: { format: 'json' }
+  resources :responders, except: [:new, :edit, :destroy], constraints: { format: 'json' }
+  resources :emergencies, except: [:new, :edit, :destroy], constraints: { format: 'json' }
 
   match '*all', to: 'application#action_not_found', via: [:get, :patch, :delete]
 end
