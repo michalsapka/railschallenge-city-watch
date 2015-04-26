ad39c9d0cafffc34adda74690043ebf9a7777be1
Michal Sapka <msapka@gmail.com>
2015-04-26 23:15:46 +0200

    comment fix

--- app/services/responder_dispatcher.rb
+++ app/services/responder_dispatcher.rb
@@ -19,13 +19,13 @@ class ResponderDispatcher
 
   private
 
-  # Assign next avalible Responder with the closes capacicy for emergency_type to current emergency
   def dispatch_responders
     dispatch_for(:police) if @severity[:police] > 0
     dispatch_for(:medical) if @severity[:medical] > 0
     dispatch_for(:fire) if @severity[:fire] > 0
   end
 
+  # Assign next avalible Responder with the closes capacicy for emergency_type to current emergency
   def dispatch_for(emergency_type)
     responder = Responder.next_avalible_for(emergency_type.capitalize, @severity[emergency_type]).first
 
